772c9cbe09aba9dbe001858fe1f7502dc4c9d725
ruygarcia <rrgarciach@gmail.com>
2014-12-11 00:29:02 -0600

    Added some tests.

---
+++ www/js/tests/tests.js
@@ -0,0 +1,581 @@
+describe("Let me see... ", function() {
+	it("lets assert something obvious", function() {
+		expect(true).toBe(true);
+	});
+});
+
+describe("Another try...", function() {
+	it("let see if toEqual works", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+});
+
+describe("Let repeat the same test over and over again ", function() {
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	it("stress test! no, I'm kidding!", function() {
+		var number = 24;
+		expect(number).toEqual(24);
+	});
+
+	
+});
