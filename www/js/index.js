e900070439c4dbe375e877750d3e298518e4b595
ruygarcia <rrgarciach@gmail.com>
2014-12-10 23:57:28 -0600

    Updated receivedEvent method in index.js

--- www/js/index.js
+++ www/js/index.js
@@ -37,12 +37,16 @@ var app = {
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
+        /*
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
 
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
+        */
+
+        window.jasminetest();
 
         console.log('Received Event: ' + id);
     }
