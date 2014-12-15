593e9b2d47f8a00d037b1fd04f07f0738950a277
ruygarcia <rrgarciach@gmail.com>
2014-12-15 09:43:06 -0600

    Screen transition functionality added.

--- www/js/index.js
+++ www/js/index.js
@@ -33,7 +33,8 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+        // app.receivedEvent('deviceready');
+        document.getElementById('buttonMenu').ontouchstart = app.displayMenu;
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
@@ -45,6 +46,9 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
+    },
+    displayMenu: function() {
+        document.getElementById('menu').className = 'page transition menu';
     }
 };
 
